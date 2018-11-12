b452faf7405f10c73698a3cba04890d808479710
Håken Lid <haakenlid@gmail.com>
2018-11-12 22:59:22 +0100

    add some libraries

--- install-jupyter.sh
+++ install-jupyter.sh
@@ -4,19 +4,26 @@
 # globally install jupyter
 python -m pip install --no-cache-dir \
   jupyterlab \
-  pillow\
   tqdm \
+  matplotlib \
+  seaborn \
   numpy \
-  opencv-python-headless 
+  pandas \
+  pillow \
+  opencv-python-headless \
+  ;
 
 # install nodejs (required for building jupyterlab extensions)
 wget -qO- https://deb.nodesource.com/setup_10.x | bash -
 apt-get install nodejs -y 
 
-# install jupyter vim extension
-jupyter labextension install jupyterlab_vim
+# install jupyterlab extensions
+jupyter labextension install \
+  jupyterlab_vim \
+  @jupyter-widgets/jupyterlab-manager \
+  ;
 
 # remove nodejs and extension build artifacts
 apt-get purge nodejs -y
-cd $(jupyter lab paths | awk -F': ' '/Application/{print $2}')
-rm -rf staging /var/lib/apt/lists/*
+staging=$(jupyter lab paths | awk -F': *' '/Application/{print $2}')/staging/
+rm -rf "$staging" /var/lib/apt/lists/*
